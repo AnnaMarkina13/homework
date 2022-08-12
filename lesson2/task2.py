0649723bddffac69975a4818ea405647c32c9d0c
Anna Markina <latyshanna0@gmail.com>
2022-08-12 15:12:03 +0300

    Дополнения к заданиям 2 урока.

--- lesson2/task2.py
+++ lesson2/task2.py
@@ -8,3 +8,17 @@ for i in range(0, len(my_list) - 1, 2):
     my_list[i] = my_list[i + 1]
     my_list[i + 1] = temp_item
 print(my_list)
+
+"""Варианты решения"""
+for i in range(1, len(my_list), 2):
+    my_list[i - 1], my_list[i] = my_list[i], my_list[i - 1]
+print(my_list)
+
+idx = len(my_list) if len(my_list) % 2 == 0 else len(my_list) - 1
+my_list[:idx:2], my_list[1:idx:2] = my_list[1:idx:2], my_list[:idx:2]
+print(my_list)
+
+for i in range(1, len(my_list), 2):
+    my_list.insert(i - 1, my_list.pop(i))
+print(my_list)
+
